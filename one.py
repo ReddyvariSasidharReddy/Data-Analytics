bb2fb96755c261d650faa9a039e848d5813b1a8a
ReddyvariSasidharReddy <sasidharreddy882@gmailcom>
2025-03-06 11:42:08 +0530

    one.py

---
+++ one.py
@@ -0,0 +1,4 @@
+# Calculate the sum of even numbers from 1 to 100
+sum_even_numbers = sum(number for number in range(1, 101) if number % 2 == 0)
+
+print("Sum of even numbers from 1 to 100:", sum_even_numbers)
